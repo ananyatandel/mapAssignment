1d706b60a140fdecbce5c25056c04515f8d9d530
Ananya Tandel <anta1515@colorado.edu>
2023-10-11 16:28:06 -0600

    Initial Commit

---
+++ mapAssignment/mapAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapAssignmentApp.swift
+//  mapAssignment
+//
+//  Created by Ananya Tandel on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct mapAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
